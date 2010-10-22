1dde8061cb960488811b72964951fa229e151802
Pete Jemian <jemian@anl.gov>
2010-10-22 19:42:01 +0000

    refs #132: adjust links and text in h5py example
    git-svn-id: https://svn.nexusformat.org/definitions/trunk@666 e2afd93b-eb5f-4fc1-b239-527b97798288

--- manual/examples/h5py/BasicReader.py
+++ manual/examples/h5py/BasicReader.py
@@ -3,15 +3,13 @@
 
 import h5py    # HDF5 support
 
-
-if __name__ == '__main__':
-    fileName = "prj_test.nexus.hdf5"
-    f = h5py.File(fileName, "r")
-    for item in f.attrs.keys():
-        print item + ":", f.attrs[item]
-    mr = f['/entry/mr_scan/mr']
-    i00 = f['/entry/mr_scan/I00']
-    print "%s\t%s\t%s" % ("#", "mr", "I00")
-    for i in range(len(mr)):
-        print "%d\t%g\t%d" % (i, mr[i], i00[i])
-    f.close()
+fileName = "prj_test.nexus.hdf5"
+f = h5py.File(fileName, "r")
+for item in f.attrs.keys():
+    print item + ":", f.attrs[item]
+mr = f['/entry/mr_scan/mr']
+i00 = f['/entry/mr_scan/I00']
+print "%s\t%s\t%s" % ("#", "mr", "I00")
+for i in range(len(mr)):
+    print "%d\t%g\t%d" % (i, mr[i], i00[i])
+f.close()
--- manual/examples/h5py/BasicWriter.py
+++ manual/examples/h5py/BasicWriter.py
@@ -3,49 +3,46 @@
 
 import h5py    # HDF5 support
 
-if __name__ == '__main__':
-    print "Write a NeXus HDF5 file"
-    fileName = "prj_test.nexus.hdf5"
-    timestamp = "2010-10-18T17:17:04-0500"
+print "Write a NeXus HDF5 file"
+fileName = "prj_test.nexus.hdf5"
+timestamp = "2010-10-18T17:17:04-0500"
 
-    # prepare the data
-    f = open('input.dat', 'r')
-    TWO_COLUMNS = f.read()
-    f.close()
-    data = {'mr': [], 'I00': []}
-    buffer = TWO_COLUMNS.strip().split("\n")
-    for row in buffer:
-        (x, y) = row.split()
-        data['mr'].append(float(x))
-        data['I00'].append(int(y))
+# prepare the data
+f = open('input.dat', 'r')
+TWO_COLUMNS = f.read()
+f.close()
 
-    # create the HDF5 NeXus file
-    f = h5py.File(fileName, "w")
-    f.attrs['file_name'] = fileName
-    f.attrs['file_time'] = timestamp
-    f.attrs['creator'] = "$Id$"
-    f.attrs['HDF5_Version'] = h5py.version.hdf5_version
-    f.attrs['NeXus_version'] = "4.3.0"
-    f.attrs['h5py_version'] = h5py.version.version
-    f.attrs['instrument'] = "APS USAXS at 32ID-B"
+data = {'mr': [], 'I00': []}
+buffer = TWO_COLUMNS.strip().split("\n")
+for row in buffer:
+    (x, y) = row.split()
+    data['mr'].append(float(x))
+    data['I00'].append(int(y))
 
-    nxentry = f.create_group("entry")
-    nxentry.attrs["NX_class"] = "NXentry"   # identify NeXus base class
+# create the HDF5 NeXus file
+f = h5py.File(fileName, "w")
+f.attrs['file_name'] = fileName
+f.attrs['file_time'] = timestamp
+f.attrs['instrument'] = "APS USAXS at 32ID-B"
+f.attrs['creator'] = "$Id$"
+f.attrs['NeXus_version'] = "4.3.0"
+f.attrs['HDF5_Version'] = h5py.version.hdf5_version
+f.attrs['h5py_version'] = h5py.version.version
 
-    # store the scan data
-    nxdata = nxentry.create_group("mr_scan")
-    nxdata.attrs["NX_class"] = "NXdata"   # identify NeXus base class
+nxentry = f.create_group("entry")
+nxentry.attrs["NX_class"] = "NXentry"   # identify NeXus base class
 
-    mr = nxdata.create_dataset("mr", data=data['mr'])
-    mr.attrs['units'] = "degrees"
-    # tell NeXus this is first independent axis for plotting
-    mr.attrs['primary'] = "1"    
+nxdata = nxentry.create_group("mr_scan")
+nxdata.attrs["NX_class"] = "NXdata"   # identify NeXus base class
 
-    i00 = nxdata.create_dataset("I00", data=data['I00'])
-    i00.attrs['units'] = "counts"
-    # tell NeXus this is primary data for plotting
-    i00.attrs['signal'] = "1"
+mr = nxdata.create_dataset("mr", data=data['mr'])
+mr.attrs['units'] = "degrees"
+mr.attrs['primary'] = "1"    # tell NeXus this is X axis for plotting
 
-    # be CERTAIN to close the file
-    f.close()
+i00 = nxdata.create_dataset("I00", data=data['I00'])
+i00.attrs['units'] = "counts"
+i00.attrs['signal'] = "1"    # tell NeXus this is Y axis for plotting
 
+# be CERTAIN to close the file
+f.close()
+print "wrote file:", fileName
