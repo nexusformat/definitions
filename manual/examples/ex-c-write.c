c8b5ad01c3ec7e58418f20d5b13431e455ce67bf
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2011-10-20 22:18:46 +0000

    fix simple c example
    refs #194
    
    git-svn-id: https://svn.nexusformat.org/definitions/trunk@924 e2afd93b-eb5f-4fc1-b239-527b97798288

--- manual/examples/ex-c-write.c
+++ manual/examples/ex-c-write.c
@@ -3,7 +3,7 @@
  int main()
  {
     NXhandle fileID;
-    NXopen ('NXfile.nxs', NXACC_CREATE, &fileID);
+    NXopen ("NXfile.nxs", NXACC_CREATE, &fileID);
       NXmakegroup (fileID, "Scan", "NXentry");
       NXopengroup (fileID, "Scan", "NXentry");
         NXmakegroup (fileID, "data", "NXdata");
@@ -13,7 +13,8 @@
           NXopendata (fileID, "two_theta");
             NXputdata (fileID, tth);
             NXputattr (fileID, "units", "degrees", 7, NX_CHAR);
-          NXclosedata (fileID);  /* two_theta */, NX_INT32, 1, &n);
+          NXclosedata (fileID);  /* two_theta */
+          NXmakedata (fileID, "counts", NX_FLOAT32, 1, &n);
           NXopendata (fileID, "counts");
             NXputdata (fileID, counts);
           NXclosedata (fileID);  /* counts */
