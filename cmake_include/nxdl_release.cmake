2b893ab2aba3f90df93fa88a7471ca7bd8226733
Pete Jemian <jemian@anl.gov>
2012-02-14 20:51:32 +0000

    refs #217, note and list the consequences of changing the NXDL version numbers
    git-svn-id: https://svn.nexusformat.org/definitions/trunk@1051 e2afd93b-eb5f-4fc1-b239-527b97798288

--- cmake_include/nxdl_release.cmake
+++ cmake_include/nxdl_release.cmake
@@ -1,6 +1,18 @@
 ##
 ## change these to specify nxdl release
 ##
+## Be __VERY__ judicious about updating NXDL_MAJOR_RELEASE and/or NXDL_MINOR_RELEASE
+##  They are used to define the XML namespace for the NXDL files.  
+##  This namespace is used in:
+##    * NXDL documents
+##    * NeXus data file validation
+##    * XSLT transformations
+##    * XSD specifications
+##    * SCH specifications
+##    * client source code files
+##    * examples in the manual
+##    * possibly other places, as well
+##
 set(NXDL_MAJOR_RELEASE 3)
 set(NXDL_MINOR_RELEASE 1)
 #set(NXDL_NAMESPACE_BASE "http://definition.nexusformat.org/nxdl")
