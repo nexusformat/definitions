080f002c58816730b7e2de9de57d8ae0f16ff3cc
Pete Jemian <jemian@anl.gov>
2012-09-25 01:37:03 +0000

    refs #240
    git-svn-id: https://svn.nexusformat.org/definitions/trunk@1159 e2afd93b-eb5f-4fc1-b239-527b97798288

--- manual/source/conf.py
+++ manual/source/conf.py
@@ -129,7 +129,7 @@ if nxdl_version is not None:
 #today = ''
 # Else, today_fmt is used as the format for a strftime call.
 #today_fmt = '%B %d, %Y'
-today_fmt = '%Y-%m-%d %H:%M %Z'
+today_fmt = '%Y-%m-%d %H:%M:%S %Z'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
