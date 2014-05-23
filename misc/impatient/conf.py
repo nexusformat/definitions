0c80779bebb780e0ba7c2db238d578090a9100af
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2014-05-23 10:50:30 +0100

    update copyright dates

--- misc/impatient/conf.py
+++ misc/impatient/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'NeXus for the Impatient'
-copyright = u'2012, http://nexusformat.org'
+copyright = u'2014, http://nexusformat.org'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
