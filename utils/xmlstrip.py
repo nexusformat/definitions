a796bcbbfe16f1dc43b4fac9f8c0c1f0e8d26ff2
prjemian <prjemian@gmail.com>
2015-05-05 13:19:04 -0500

    do not add \n for each line of output, update the docstring

--- utils/xmlstrip.py
+++ utils/xmlstrip.py
@@ -3,12 +3,14 @@
 # Tested under both python2 and python3.
 
 '''
-Read the NeXus NXDL class specification and describe it.  
-Write a restructured text (.rst) document for use in the NeXus manual in 
-the NeXus NXDL Classes chapter.
-'''
+Strip certain XML declarations from an XML file.
 
-# testing:  see file dev_nxdl2rst.py
+Read a NeXus NXDL class specification and write it to stdout,
+stripping out these XML declarations:
+
+* ``<?xml version``
+* ``<?xml stylesheet``
+'''
 
 from __future__ import print_function
 import os, sys, re
@@ -17,7 +19,7 @@ version = re.compile(r"^<\?xml version")
 stylesheet = re.compile(r"^<\?xml-stylesheet")
 
 with open(sys.argv[1],"r") as in_file:
-    for line in in_file:
+    for line in in_file.read().splitlines():
 
         if version.match(line) or stylesheet.match(line):
             continue
