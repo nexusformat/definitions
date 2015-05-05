b90c247d8b3a09cfc06be5337de6e56ca228607c
prjemian <prjemian@gmail.com>
2015-05-05 18:16:34 -0500

    important note added as comment

--- utils/xmlstrip.py
+++ utils/xmlstrip.py
@@ -21,6 +21,19 @@ stylesheet = re.compile(r"^<\?xml-stylesheet")
 with open(sys.argv[1],"r") as in_file:
     for line in in_file.read().splitlines():
 
+        # Note:
+        # This next test assumes that XML declarations are the only text 
+        # on their respective lines in the .nxdl.xml file.
+        # If the line also includes an opening of a new tag, this code 
+        # will break that opening and the resulting .sch file will be invalid.
+        #
+        # wrong:
+        #     <?xml-stylesheet type="text/xsl" href="nxdlformat.xsl" ?><!--
+        #
+        # correct:
+        #     <?xml-stylesheet type="text/xsl" href="nxdlformat.xsl" ?>
+        #     <!--
+        
         if version.match(line) or stylesheet.match(line):
             continue
 
