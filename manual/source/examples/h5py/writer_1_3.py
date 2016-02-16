4c74f464639c53d1ae2827fce46fcf09ec8ececf
Pete R. Jemian <prjemian@gmail.com>
2016-02-16 10:56:46 -0600

    refs #442: this conversion already exists

--- manual/source/examples/h5py/writer_1_3.py
+++ manual/source/examples/h5py/writer_1_3.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python
 '''
-Writes the simplest NeXus HDF5 file using 
-a simple helper library with h5py and numpy calls
+Writes the simplest NeXus HDF5 file using h5py 
+
+Uses method accepted at 2014NIAC
 according to the example from Figure 1.3 
 in the Introduction chapter
 '''
 
-import my_lib
+import h5py
+import numpy
 
 INPUT_FILE = 'input.dat'
-HDF5_FILE = 'writer_1_3.hdf5'
+HDF5_FILE = 'writer_1_3_h5py.hdf5'
 
 #---------------------------
 
-tthData, countsData = my_lib.get2ColumnData(INPUT_FILE)
+tthData, countsData = numpy.loadtxt(INPUT_FILE).T
 
-f = my_lib.makeFile(HDF5_FILE)
+f = h5py.File(HDF5_FILE, "w")  # create the HDF5 NeXus file
 # since this is a simple example, no attributes are used at this point
 
-nxentry = my_lib.makeGroup(f, 'Scan', 'NXentry')
-nxdata = my_lib.makeGroup(nxentry, 'data', 'NXdata', 
-                          signal='counts', 
-                          axes='two_theta',
-                          two_theta_indices = [0,],
-                          )
+nxentry = f.create_group('Scan')
+nxentry.attrs["NX_class"] = 'NXentry'
 
-my_lib.makeDataset(nxdata, "two_theta", tthData, units='degrees')
-my_lib.makeDataset(nxdata, "counts", countsData, units='counts')
+nxdata = nxentry.create_group('data')
+nxdata.attrs["NX_class"] = 'NXdata'
+nxdata.attrs['signal'] = "counts"
+nxdata.attrs['axes'] = "two_theta"
+nxdata.attrs['two_theta_indices'] = [0,]
+
+tth = nxdata.create_dataset("two_theta", data=tthData)
+tth.attrs['units'] = "degrees"
+
+counts = nxdata.create_dataset("counts", data=countsData)
+counts.attrs['units'] = "counts"
 
 f.close()	# be CERTAIN to close the file
