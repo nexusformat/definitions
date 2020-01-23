77c677523bcff8bac84b90d32b742b978690d33c
ISIS Jenkins <freddie.akeroyd@stfc.ac.uk>
2020-01-23 20:23:56 +0000

    Update docs for 4f6ef8d5

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2018.5',
+    VERSION: 'v2020.1rc1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
