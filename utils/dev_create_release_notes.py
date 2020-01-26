ac8317a9d65d0f33146dd6561bd1ae6c99ff5bf9
Pete Jemian <prjemian@gmail.com>
2020-01-26 11:38:49 -0600

    TST #728 developer testing

--- utils/dev_create_release_notes.py
+++ utils/dev_create_release_notes.py
@@ -3,6 +3,21 @@
 '''
 Developers: use this code to develop and test create_release_notes.py
 '''
+import os
+
+CREDS_FILE = os.path.join(
+    os.environ["HOME"],
+    ".config",
+    "github_token",
+)
+
+with open(CREDS_FILE, "r") as cf:
+    token = cf.read().strip()
 
 from create_release_notes import main
-main("NXDL 2018.5", head="master", milestone="NXDL 2020.1")
+main(
+    base="v2018.5", 
+    head="master", 
+    milestone="NXDL 2020.1", 
+    token=token,
+    debug=True)
