3874cc8e604c1f02665e8cb7572b82e1e3c47a4b
Herbert J. Bernstein <yayahjb@gmail.com>
2020-01-28 00:31:54 -0500

    Resolve some differences with Gold Standard paper -- HJB

--- manual/source/conf.py
+++ manual/source/conf.py
@@ -36,7 +36,7 @@ if nxdl_version is not None:
 # -- General configuration ---------------------------------------------------
 
 # https://github.com/nexusformat/definitions/issues/659#issuecomment-577438319
-needs_sphinx = '2.3'
+needs_sphinx = '1.8'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
