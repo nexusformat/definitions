1138ebfe0088cf56e8505244e5419646db9ee76e
Sandor Brockhauser <sandor.brockhauser@xfel.eu>
2023-06-15 23:56:12 +0200

    short tooltip and link

--- dev_tools/docs/nxdl.py
+++ dev_tools/docs/nxdl.py
@@ -668,7 +668,7 @@ class NXClassDocGenerator:
                 )
             parent_display_name = f"{parent_def_name[1:]}{parent_path}"
             return (
-                f"<:abbr:`parent (parent definition: {parent_display_name})"
-                + f"`:ref:`🔗 </{parent_display_name}-{tag}>`>"
+                f":abbr:`... (override: {parent_display_name})"
+                + f"`:ref:`🔗 </{parent_display_name}-{tag}>`"
             )
         return ""
