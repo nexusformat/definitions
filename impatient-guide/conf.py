cd1cf3fff0e5a164fb0877df7fb0c2543711ff85
Peter Chang <peter.chang@diamond.ac.uk>
2023-06-19 14:55:13 +0100

    Use f-string

--- impatient-guide/conf.py
+++ impatient-guide/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 # General information about the project.
 year = '2022' # str(datetime.datetime.now().year)
 project = u'NeXus for the Impatient'
-copyright = u'2014-{}, http://nexusformat.org'.format(year)
+copyright = f'2014-{year}, http://nexusformat.org'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
