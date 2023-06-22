f3bc402d6c62b94fc6104ab5e8ac1d3da5ef3aa5
markus.kuehbach <markus.kuehbach@hu-berlin.de>
2023-06-22 12:20:48 +0200

    Added guide to base classes and application definitions for computational-geometry-based microstructures

--- dev_tools/docs/nxdl_index.py
+++ dev_tools/docs/nxdl_index.py
@@ -72,6 +72,8 @@ def nxdl_indices() -> Dict[str, dict]:
             rst_lines.append(f"{indentation}mpes-structure\n")
             rst_lines.append(f"{indentation}apm-structure\n")
             rst_lines.append(f"{indentation}transport-structure\n")
+            rst_lines.append(f"{indentation}cgms-structure\n")
+
         for cname in sorted(classes):
             rst_lines.append(f"{indentation}{cname}\n")
 
