f0d77e07feade3f008205dd195bd0c28df401d52
Russ Berg <russ.berg@lightsource.ca>
2023-06-22 10:54:52 -0600

    compiled re instead of passing string expression each time

--- dev_tools/ext/contrib_ext.py
+++ dev_tools/ext/contrib_ext.py
@@ -6,21 +6,23 @@ import re
 # that has been over ridden and sits in the _templates directory to produce the
 # contributor information on the for right sidebar of the html pages
 
+variables_re = re.compile(r"\|(\w+)\| replace::\s(.+)")
 def extract_contributor_vars(app, docname, source):
     # Read the RST file content
     content = source[0]
 
     # Extract variables using regular expressions
-    variables = re.findall(r'\|(\w+)\| replace::\s(.+)', content)
+    variables = variables_re.findall(content)
 
     # Create a dictionary to store the extracted variables
     # this will create a list of single strings each of which contain the info about the contributor
     extracted_variables = [var[1] for var in variables]
     if "variables" not in app.config.html_context.keys():
-        app.config.html_context['variables'] = {}
+        app.config.html_context["variables"] = {}
 
     # Add the extracted variables to the Jinja environment
-    app.config.html_context['variables'][docname] = extracted_variables
+    app.config.html_context["variables"][docname] = extracted_variables
+
 
 def setup(app):
     app.connect("source-read", extract_contributor_vars)
