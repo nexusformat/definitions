4d3d8f5065a10183fac3ccf5f4ca132693c196d9
Rubel <rubel.mozumder@outlook.com>
2023-09-06 11:55:08 +0200

    changes from Peter and Pete.

--- dev_tools/nyaml2nxdl/nyaml2nxdl.py
+++ dev_tools/nyaml2nxdl/nyaml2nxdl.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""Main file of nyaml2nxdl tool.
-Users create NeXus instances by writing a YAML file
-which details a hierarchy of data/metadata elements
+"""
+Main file of nyaml2nxdl tool.
 
+To write a definition for a instrument, experiment and/or process in nxdl.xml file from a YAML
+file which details a hierarchy of data/metadata elements. It also allows both way 
+conversion beteen YAML and nxdl.xml files that follows rules of NeXus ontology or data format.
 """
 # -*- coding: utf-8 -*-
 #
@@ -23,6 +25,7 @@ which details a hierarchy of data/metadata elements
 # limitations under the License.
 #
 import os
+from pathlib import Path
 import xml.etree.ElementTree as ET
 
 import click
@@ -36,6 +39,7 @@ from .nyaml2nxdl_helper import get_sha256_hash
 from .nyaml2nxdl_helper import separate_hash_yaml_and_nxdl
 
 DEPTH_SIZE = 4 * " "
+_nxdl = ".nxdl.xml"
 
 # NOTE: Some handful links for nyaml2nxdl converter:
 # https://manual.nexusformat.org/nxdl_desc.html?highlight=optional
@@ -44,7 +48,8 @@ DEPTH_SIZE = 4 * " "
 def generate_nxdl_or_retrieve_nxdl(yaml_file, out_xml_file, verbose):
     """
     Generate yaml, nxdl and hash.
-    if the extracted hash is exactly the same as producd from generated yaml then
+
+    If the extracted hash is exactly the same as producd from generated yaml then
     retrieve the nxdl part from provided yaml.
     Else, generate nxdl from separated yaml with the help of nyaml2nxdl function
     """
@@ -55,117 +60,115 @@ def generate_nxdl_or_retrieve_nxdl(yaml_file, out_xml_file, verbose):
     if hash_found:
         gen_hash = get_sha256_hash(sep_yaml)
         if hash_found == gen_hash:
-            os.remove(sep_yaml)
+            Path(sep_yaml).unlink()
+            # os.remove(sep_yaml)
             return
 
     nyaml2nxdl(sep_yaml, out_xml_file, verbose)
-    os.remove(sep_yaml)
+    Path(sep_yaml).unlink()
+    # os.remove(sep_yaml)
 
 
-# pylint: disable=too-many-locals
-def append_yml(input_file, append, verbose):
-    """Append to an existing NeXus base class new elements provided in YML input file \
-and print both an XML and YML file of the extended base class.
+# # pylint: disable=too-many-locals
+# def append_yml(input_file, append, verbose):
+#     """
+#     Append to an existing NeXus base class new elements provided in YML input file.
+#     """
+#     nexus_def_path = os.path.join(
+#         os.path.abspath(os.path.dirname(__file__)), "../../definitions"
+#     )
+#     assert [
+#         s
+#         for s in os.listdir(os.path.join(nexus_def_path, "base_classes"))
+#         if append.strip() == s.replace(_nxdl, "")
+#     ], "Your base class extension does not match any existing NeXus base classes"
+#     tree = ET.parse(
+#         os.path.join(nexus_def_path + "/base_classes", append + _nxdl)
+#     )
+#     root = tree.getroot()
+#     # warning: tmp files are printed on disk and removed at the ends!!
+#     pretty_print_xml(root, "tmp.nxdl.xml")
+#     input_tmp_xml = "tmp.nxdl.xml"
+#     out_tmp_yml = "tmp_parsed.yaml"
+#     converter = Nxdl2yaml([], [])
+#     converter.print_yml(input_tmp_xml, out_tmp_yml, verbose)
+#     nyaml2nxdl(input_file=out_tmp_yml, out_file="tmp_parsed.nxdl.xml", verbose=verbose)
+#     tree = ET.parse("tmp_parsed.nxdl.xml")
+#     tree2 = ET.parse(input_file)
+#     root_no_duplicates = ET.Element(
+#         "definition",
+#         {
+#             "xmlns": "http://definition.nexusformat.org/nxdl/3.1",
+#             "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+#             "xsi:schemaLocation": "http://www.w3.org/2001/XMLSchema-instance",
+#         },
+#     )
+#     for attribute_keys in root.attrib.keys():
+#         if (
+#             attribute_keys
+#             != "{http://www.w3.org/2001/XMLSchema-instance}schemaLocation"
+#         ):
+#             attribute_value = root.attrib[attribute_keys]
+#             root_no_duplicates.set(attribute_keys, attribute_value)
+#     for elems in root.iter():
+#         if "doc" in elems.tag:
+#             root_doc = ET.SubElement(root_no_duplicates, "doc")
+#             root_doc.text = elems.text
+#             break
+#     group = "{http://definition.nexusformat.org/nxdl/3.1}group"
+#     root_no_duplicates = compare_niac_and_my(
+#         tree, tree2, verbose, group, root_no_duplicates
+#     )
+#     field = "{http://definition.nexusformat.org/nxdl/3.1}field"
+#     root_no_duplicates = compare_niac_and_my(
+#         tree, tree2, verbose, field, root_no_duplicates
+#     )
+#     attribute = "{http://definition.nexusformat.org/nxdl/3.1}attribute"
+#     root_no_duplicates = compare_niac_and_my(
+#         tree, tree2, verbose, attribute, root_no_duplicates
+#     )
+#     pretty_print_xml(
+#         root_no_duplicates,
+#         f"{input_file.replace('.nxdl.xml', '')}" f"_appended.nxdl.xml",
+#     )
 
-"""
-    nexus_def_path = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), "../../definitions"
-    )
-    assert [
-        s
-        for s in os.listdir(os.path.join(nexus_def_path, "base_classes"))
-        if append.strip() == s.replace(".nxdl.xml", "")
-    ], "Your base class extension does not match any existing NeXus base classes"
-    tree = ET.parse(
-        os.path.join(nexus_def_path + "/base_classes", append + ".nxdl.xml")
-    )
-    root = tree.getroot()
-    # warning: tmp files are printed on disk and removed at the ends!!
-    pretty_print_xml(root, "tmp.nxdl.xml")
-    input_tmp_xml = "tmp.nxdl.xml"
-    out_tmp_yml = "tmp_parsed.yaml"
-    converter = Nxdl2yaml([], [])
-    converter.print_yml(input_tmp_xml, out_tmp_yml, verbose)
-    nyaml2nxdl(input_file=out_tmp_yml, out_file="tmp_parsed.nxdl.xml", verbose=verbose)
-    tree = ET.parse("tmp_parsed.nxdl.xml")
-    tree2 = ET.parse(input_file)
-    root_no_duplicates = ET.Element(
-        "definition",
-        {
-            "xmlns": "http://definition.nexusformat.org/nxdl/3.1",
-            "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-            "xsi:schemaLocation": "http://www.w3.org/2001/XMLSchema-instance",
-        },
-    )
-    for attribute_keys in root.attrib.keys():
-        if (
-            attribute_keys
-            != "{http://www.w3.org/2001/XMLSchema-instance}schemaLocation"
-        ):
-            attribute_value = root.attrib[attribute_keys]
-            root_no_duplicates.set(attribute_keys, attribute_value)
-    for elems in root.iter():
-        if "doc" in elems.tag:
-            root_doc = ET.SubElement(root_no_duplicates, "doc")
-            root_doc.text = elems.text
-            break
-    group = "{http://definition.nexusformat.org/nxdl/3.1}group"
-    root_no_duplicates = compare_niac_and_my(
-        tree, tree2, verbose, group, root_no_duplicates
-    )
-    field = "{http://definition.nexusformat.org/nxdl/3.1}field"
-    root_no_duplicates = compare_niac_and_my(
-        tree, tree2, verbose, field, root_no_duplicates
-    )
-    attribute = "{http://definition.nexusformat.org/nxdl/3.1}attribute"
-    root_no_duplicates = compare_niac_and_my(
-        tree, tree2, verbose, attribute, root_no_duplicates
-    )
-    pretty_print_xml(
-        root_no_duplicates,
-        f"{input_file.replace('.nxdl.xml', '')}" f"_appended.nxdl.xml",
-    )
-
-    input_file_xml = input_file.replace(".nxdl.xml", "_appended.nxdl.xml")
-    out_file_yml = input_file.replace(".nxdl.xml", "_appended_parsed.yaml")
-    converter = Nxdl2yaml([], [])
-    converter.print_yml(input_file_xml, out_file_yml, verbose)
-    nyaml2nxdl(
-        input_file=out_file_yml,
-        out_file=out_file_yml.replace(".yaml", ".nxdl.xml"),
-        verbose=verbose,
-    )
-    os.rename(
-        f"{input_file.replace('.nxdl.xml', '_appended_parsed.yaml')}",
-        f"{input_file.replace('.nxdl.xml', '_appended.yaml')}",
-    )
-    os.rename(
-        f"{input_file.replace('.nxdl.xml', '_appended_parsed.nxdl.xml')}",
-        f"{input_file.replace('.nxdl.xml', '_appended.nxdl.xml')}",
-    )
-    os.remove("tmp.nxdl.xml")
-    os.remove("tmp_parsed.yaml")
-    os.remove("tmp_parsed.nxdl.xml")
+#     input_file_xml = input_file.replace(_nxdl, "_appended.nxdl.xml")
+#     out_file_yml = input_file.replace(_nxdl, "_appended_parsed.yaml")
+#     converter = Nxdl2yaml([], [])
+#     converter.print_yml(input_file_xml, out_file_yml, verbose)
+#     nyaml2nxdl(
+#         input_file=out_file_yml,
+#         out_file=out_file_yml.replace(".yaml", _nxdl),
+#         verbose=verbose,
+#     )
+#     os.rename(
+#         f"{input_file.replace('.nxdl.xml', '_appended_parsed.yaml')}",
+#         f"{input_file.replace('.nxdl.xml', '_appended.yaml')}",
+#     )
+#     os.rename(
+#         f"{input_file.replace('.nxdl.xml', '_appended_parsed.nxdl.xml')}",
+#         f"{input_file.replace('.nxdl.xml', '_appended.nxdl.xml')}",
+#     )
+#     # delete the files
+#     Path("tmp.nxdl.xml").unlink()
+#     # os.remove("tmp.nxdl.xml")
+#     Path("tmp_parsed.yaml").unlink()
+#     # os.remove("tmp_parsed.yaml")
+#     Path("tmp_parsed.nxdl.xml").unlink()
+#     # os.remove("tmp_parsed.nxdl.xml")
 
 
-def split_name_and_extension(file_name):
+def split_name_and_extension(file_path):
     """
     Split file name into extension and rest of the file name.
-    return file raw nam and extension
+
+    return file raw name and extension
     """
-    path = file_name.rsplit("/", 1)
-    (pathn, filen) = ["", path[0]] if len(path) == 1 else [path[0] + "/", path[1]]
-    parts = filen.rsplit(".", 2)
-    raw = ext = ""
-    if len(parts) == 2:
-        raw = parts[0]
-        ext = parts[1]
-    elif len(parts) == 3:
-        raw = parts[0]
-        ext = ".".join(parts[1:])
-
-    return pathn + raw, ext
-
+    path = Path(file_path)
+    ext = ''.join(path.suffixes)
+    full_path_stem = file_path[0 : file_path.index(ext)]
+    return full_path_stem, ext[1:]
+    
 
 @click.command()
 @click.option(
@@ -175,17 +178,17 @@ def split_name_and_extension(file_name):
     help="The path to the XML or YAML input data file to read and create \
 a YAML or XML file from, respectively.",
 )
-@click.option(
-    "--append",
-    help="Parse xml file and append to base class, given that the xml file has same name \
-of an existing base class",
-)
+# @click.option(
+#     "--append",
+#     help="Parse xml file and append to base class, given that the xml file has same name \
+# of an existing base class",
+# )
 @click.option(
     "--check-consistency",
     is_flag=True,
     default=False,
     help=(
-        "Check wether yaml or nxdl has followed general rules of scema or not"
+        "Check if yaml or nxdl has followed general rules of whether schema or not"
         "check whether your comment in the right place or not. The option render an "
         "output file of the same extension(*_consistency.yaml or *_consistency.nxdl.xml)"
     ),
@@ -197,9 +200,9 @@ of an existing base class",
     help="Print in standard output keywords and value types to help \
 possible issues in yaml files",
 )
-def launch_tool(input_file, verbose, append, check_consistency):
+def launch_tool(input_file, verbose, check_consistency):
     """
-    Main function that distiguishes the input file format and launches the tools.
+    Main function that distinguishes the input file format and launches the tools.
     """
     if os.path.isfile(input_file):
         raw_name, ext = split_name_and_extension(input_file)
@@ -207,44 +210,46 @@ def launch_tool(input_file, verbose, append, check_consistency):
         raise ValueError("Need a valid input file.")
 
     if ext == "yaml":
-        xml_out_file = raw_name + ".nxdl.xml"
+        xml_out_file = raw_name + _nxdl
         generate_nxdl_or_retrieve_nxdl(input_file, xml_out_file, verbose)
-        if append:
-            append_yml(raw_name + ".nxdl.xml", append, verbose)
+        # if append:
+        #     append_yml(raw_name + _nxdl, append, verbose)
         # For consistency running
         if check_consistency:
             yaml_out_file = raw_name + "_consistency." + ext
             converter = Nxdl2yaml([], [])
             converter.print_yml(xml_out_file, yaml_out_file, verbose)
-            os.remove(xml_out_file)
+            Path(xml_out_file).unlink()
+            # os.remove(xml_out_file)
     elif ext == "nxdl.xml":
-        if not append:
-            yaml_out_file = raw_name + "_parsed" + ".yaml"
-            converter = Nxdl2yaml([], [])
-            converter.print_yml(input_file, yaml_out_file, verbose)
-            # Append nxdl.xml file with yaml output file
-            yaml_hash = get_sha256_hash(yaml_out_file)
-            # Lines as divider between yaml and nxdl
-            top_lines = [
-                (
-                    "\n# ++++++++++++++++++++++++++++++++++ SHA HASH"
-                    " ++++++++++++++++++++++++++++++++++\n"
-                ),
-                f"# {yaml_hash}\n",
-            ]
+        # if not append:
+        yaml_out_file = raw_name + "_parsed" + ".yaml"
+        converter = Nxdl2yaml([], [])
+        converter.print_yml(input_file, yaml_out_file, verbose)
+        # Append nxdl.xml file with yaml output file
+        yaml_hash = get_sha256_hash(yaml_out_file)
+        # Lines as divider between yaml and nxdl
+        top_lines = [
+            (
+                "\n# ++++++++++++++++++++++++++++++++++ SHA HASH"
+                " ++++++++++++++++++++++++++++++++++\n"
+            ),
+            f"# {yaml_hash}\n",
+        ]
 
-            extend_yamlfile_with_comment(
-                yaml_file=yaml_out_file,
-                file_to_be_appended=input_file,
-                top_lines_list=top_lines,
-            )
-        else:
-            append_yml(input_file, append, verbose)
+        extend_yamlfile_with_comment(
+            yaml_file=yaml_out_file,
+            file_to_be_appended=input_file,
+            top_lines_list=top_lines,
+        )
+        # else:
+        #     append_yml(input_file, append, verbose)
         # Taking care of consistency running
         if check_consistency:
             xml_out_file = raw_name + "_consistency." + ext
             generate_nxdl_or_retrieve_nxdl(yaml_out_file, xml_out_file, verbose)
-            os.remove(yaml_out_file)
+            Path.unlink(yaml_out_file)
+            # os.remove(yaml_out_file)
     else:
         raise ValueError("Provide correct file with extension '.yaml or '.nxdl.xml")
 
