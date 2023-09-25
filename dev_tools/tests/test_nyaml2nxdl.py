6ceaebd6d92937fcc98b20c5de415816d37a58b2
domna <florian.dobener@physik.hu-berlin.de>
2023-09-25 14:20:25 +0200

    Removes unused code

--- dev_tools/tests/test_nyaml2nxdl.py
+++ dev_tools/tests/test_nyaml2nxdl.py
@@ -5,16 +5,12 @@ from click.testing import CliRunner
 from ..nyaml2nxdl import nyaml2nxdl as conv
 from ..utils.nxdl_utils import find_definition_file
 
-# import subprocess
-
 
 def test_conversion():
     root = find_definition_file("NXentry")
-    # subprocess.run(["python3","-m","dev_tools.nyaml2nxdl.nyaml2nxdl","--input-file",root])
     result = CliRunner().invoke(conv.launch_tool, ["--input-file", root])
     assert result.exit_code == 0
     yaml = root[:-9] + "_parsed.yaml"
-    # subprocess.run(["python3","-m","dev_tools.nyaml2nxdl.nyaml2nxdl","--input-file",yaml])
     result = CliRunner().invoke(conv.launch_tool, ["--input-file", yaml])
     assert result.exit_code == 0
     new_root = yaml[:-4] + "nxdl.xml"
