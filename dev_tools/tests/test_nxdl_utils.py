f419a0943aba39953aa470d7f76bb4bef055610c
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-10-17 11:21:02 +0200

    add test on NXem application definition

--- dev_tools/tests/test_nxdl_utils.py
+++ dev_tools/tests/test_nxdl_utils.py
@@ -48,6 +48,37 @@ def test_get_node_at_nxdl_path():
     )
     assert node.attrib["name"] == "long_name"
 
+    nxdl_file_path = os.path.join(local_dir, "../../applications/NXem.nxdl.xml")
+    elem = ET.parse(nxdl_file_path).getroot()
+    node = nexus.get_node_at_nxdl_path(
+        "/ENTRY/measurement/EVENT_DATA_EM_SET/EVENT_DATA_EM/end_time", elem=elem
+    )
+    assert node.attrib["name"] == "end_time"
+
+    node = nexus.get_node_at_nxdl_path("/ENTRY/measurement", elem=elem)
+    assert node.attrib["type"] == "NXem_msr"
+
+    node = nexus.get_node_at_nxdl_path(
+        "/ENTRY/measurement/EVENT_DATA_EM_SET/EVENT_DATA_EM/IMAGE_SET/image_3d",
+        elem=elem,
+    )
+    assert node.attrib["type"] == "NXdata"
+
+    node = nexus.get_node_at_nxdl_path(
+        "/ENTRY/measurement/EVENT_DATA_EM_SET/EVENT_DATA_EM/IMAGE_SET/image_3d/AXISNAME_indices",
+        elem=elem,
+    )
+    assert node.attrib["name"] == "AXISNAME_indices"
+
+    node = nexus.get_node_at_nxdl_path(
+        "/ENTRY/measurement/EVENT_DATA_EM_SET/EVENT_DATA_EM/IMAGE_SET/image_3d/axis_j",
+        elem=elem,
+    )
+    assert node.attrib["type"] == "NX_NUMBER"
+
+    node = nexus.get_node_at_nxdl_path("/ENTRY/coordinate_system_set", elem=elem)
+    assert node.attrib["type"] == "NXcoordinate_system_set"
+
 
 def test_get_inherited_nodes():
     """Test to verify if we receive the right XML element list for a given NXDL path."""
