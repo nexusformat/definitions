643293d3b670e1c37d4ff1aa60cfad58fa887e72
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-12-10 17:16:01 +0100

    render open enumerations in the docs

--- dev_tools/docs/nxdl.py
+++ dev_tools/docs/nxdl.py
@@ -511,11 +511,14 @@ class NXClassDocGenerator:
         node_list = parent.xpath("nx:item", namespaces=ns)
         if len(node_list) == 0:
             return ""
-
-        if len(node_list) == 1:
-            self._print(f"{indent}Obligatory value:", end="")
+        
+        if parent.attrib.get("open", "false") == "true":
+            self._print(f"{indent}This is an open enumeration. Any of these values is suggested:", end="")
         else:
-            self._print(f"{indent}Any of these values:", end="")
+            if len(node_list) == 1:
+                self._print(f"{indent}Obligatory value:", end="")
+            else:
+                self._print(f"{indent}Any of these values:", end="")
 
         docs = OrderedDict()
         for item in node_list:
