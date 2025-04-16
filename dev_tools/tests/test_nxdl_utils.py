961e488af8c02b3c45677fb331312e732b82af66
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2025-04-16 13:05:29 +0200

    fix test in dev_tools

--- dev_tools/tests/test_nxdl_utils.py
+++ dev_tools/tests/test_nxdl_utils.py
@@ -47,38 +47,6 @@ def test_get_node_at_nxdl_path():
     )
     assert node.attrib["name"] == "long_name"
 
-    nxdl_file_path = local_dir / "../../contributed_definitions/NXem.nxdl.xml"
-
-    elem = ET.parse(nxdl_file_path).getroot()
-    node = nexus.get_node_at_nxdl_path(
-        "/ENTRY/measurement/EVENT_DATA_EM_SET/EVENT_DATA_EM/end_time", elem=elem
-    )
-    assert node.attrib["name"] == "end_time"
-
-    node = nexus.get_node_at_nxdl_path("/ENTRY/measurement", elem=elem)
-    assert node.attrib["type"] == "NXem_msr"
-
-    node = nexus.get_node_at_nxdl_path(
-        "/ENTRY/measurement/EVENT_DATA_EM_SET/EVENT_DATA_EM/IMAGE_SET/image_3d",
-        elem=elem,
-    )
-    assert node.attrib["type"] == "NXdata"
-
-    node = nexus.get_node_at_nxdl_path(
-        "/ENTRY/measurement/EVENT_DATA_EM_SET/EVENT_DATA_EM/IMAGE_SET/image_3d/AXISNAME_indices",
-        elem=elem,
-    )
-    assert node.attrib["name"] == "AXISNAME_indices"
-
-    node = nexus.get_node_at_nxdl_path(
-        "/ENTRY/measurement/EVENT_DATA_EM_SET/EVENT_DATA_EM/IMAGE_SET/image_3d/axis_j",
-        elem=elem,
-    )
-    assert node.attrib["type"] == "NX_NUMBER"
-
-    node = nexus.get_node_at_nxdl_path("/ENTRY/coordinate_system_set", elem=elem)
-    assert node.attrib["type"] == "NXcoordinate_system_set"
-
     nxdl_file_path = local_dir / "../../contributed_definitions/NXiv_temp.nxdl.xml"
 
     elem = ET.parse(nxdl_file_path).getroot()
@@ -100,7 +68,6 @@ def test_get_inherited_nodes():
 
     elem = ET.parse(nxdl_file_path).getroot()
     (_, _, elist) = nexus.get_inherited_nodes(nxdl_path="/ENTRY/NXODD_name", elem=elem)
-    print(elist)
     assert len(elist) == 5
 
     nxdl_file_path = (
